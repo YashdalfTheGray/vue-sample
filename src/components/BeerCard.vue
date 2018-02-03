45720dd5e662c973a5081f340f10e31557aaaa8a
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-02-03 18:46:11 -0500

    added some components to handle some stuff

---
+++ src/components/BeerCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <md-card id="beer-card">
+        <md-card-header>
+            <div class="md-title">{{ title }}</div>
+        </md-card-header>
+        <md-card-content>This is where information about the card will go.</md-card-content>
+        <md-card-actions>
+            <md-button>Like</md-button>
+        </md-card-actions>
+    </md-card>
+</template>
+
+<script>
+export default {
+    name: 'beer-card',
+    data() {
+        return {
+            title: 'Some Beer'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BeerGrid.vue
@@ -0,0 +1,29 @@
+<template>
+    <md-content id="beer-grid" class="beer-list">
+        <beer-card></beer-card>
+        <beer-card></beer-card>
+        <beer-card></beer-card>
+        <beer-card></beer-card>
+        <beer-card></beer-card>
+    </md-content>
+</template>
+
+<script>
+import BeerCard from './BeerCard.vue';
+
+export default {
+    name: 'beer-grid',
+    data() {
+        return {};
+    },
+    components: {
+        BeerCard
+    }
+};
+</script>
+
+<style scoped>
+    .beer-list > * + * {
+        margin-top: 8px;
+    }
+</style>
