8332f315d4ca40cd3371d39a1541dac301a2860f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-02-04 20:40:17 -0800

    added a search bar to the page

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <md-content id="beer-search">
+        <md-field>
+            <label>Search...</label>
+            <md-input v-model="query"></md-input>
+        </md-field>
+    </md-content>
+</template>
+
+<script>
+export default {
+    name: 'beer-search',
+    data() {
+        return {
+            query: ''
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
