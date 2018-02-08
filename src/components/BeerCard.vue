e20e94ba225c15713644f407017dd0d44f419d3e
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-02-07 23:39:21 -0800

    added beer stuff to the beer card

--- src/components/BeerCard.vue
+++ src/components/BeerCard.vue
@@ -1,9 +1,11 @@
 <template>
-    <md-card id="beer-card">
+    <md-card id="beer-card" v-cloak>
         <md-card-header>
-            <div class="md-title">{{ title }}</div>
+            <div class="md-title">{{ beer.name }}</div>
         </md-card-header>
-        <md-card-content>This is where information about the card will go.</md-card-content>
+        <md-card-content>
+            <div>{{ beer }}</div>
+        </md-card-content>
         <md-card-actions>
             <md-button>Like</md-button>
         </md-card-actions>
@@ -11,16 +13,25 @@
 </template>
 
 <script>
+import beerService from '../beerService';
+
 export default {
     name: 'beer-card',
     data() {
         return {
-            title: 'Some Beer'
+            beer: {}
         }
+    },
+    async created() {
+        const response = await beerService.getBeers();
+        console.log(response);
+        this.beer = response.value[0];
     }
 }
 </script>
 
 <style scoped>
-
+[v-cloak] {
+    display: none;
+}
 </style>
